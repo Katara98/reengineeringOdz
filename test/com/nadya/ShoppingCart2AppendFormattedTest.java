22ef202e57118df90ab74c492b278c1d6b36dea1
Nadya <Katara98Katara@gmail.com>
2019-12-22 13:51:04 +0200

    Task #2: Add tests - Add ShoppingCart2 class - Add tests for ShoppingCart2.appendFormatted method - Add tests for ShoppingCart2.calculateDiscount method

---
+++ test/com/nadya/ShoppingCart2AppendFormattedTest.java
@@ -0,0 +1,42 @@
+package com.nadya;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ShoppingCart2AppendFormattedTest {
+    private String value;
+    private int align;
+    private int width;
+    private String expected;
+
+    public ShoppingCart2AppendFormattedTest(String value, int align, int width, String expected) {
+        this.value = value;
+        this.align = align;
+        this.width = width;
+        this.expected = expected;
+    }
+    @Parameterized.Parameters
+    public static Collection a() {
+        return Arrays.asList(new Object[][]{
+                { "SomeLine", 0, 14, "   SomeLine    " },
+                { "SomeLine", 0, 15, "   SomeLine     " },
+                { "SomeLine", 0, 5, "SomeL " },
+                { "SomeLine", 1, 15, "       SomeLine " },
+                { "SomeLine", -1, 15, "SomeLine        " }
+        });
+    }
+    @Test
+    public void testAppendFormatted() {
+        StringBuilder sb = new StringBuilder();
+        ShoppingCart2.appendFormatted(sb, value, align, width);
+        assertEquals(expected, sb.toString());
+    }
+}
+    
---
+++ test/com/nadya/ShoppingCart2CalculateDiscountTest.java
@@ -0,0 +1,43 @@
+package com.nadya;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import static com.nadya.ShoppingCart2.ItemType;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class ShoppingCart2CalculateDiscountTest {
+    private ShoppingCart2.ItemType itemType;
+    private int quantity;
+    private int expectedDiscount;
+
+    public ShoppingCart2CalculateDiscountTest(ShoppingCart2.ItemType itemType, int quantity, int expectedDiscount) {
+        this.itemType = itemType;
+        this.quantity = quantity;
+        this.expectedDiscount = expectedDiscount;
+    }
+
+    @Parameterized.Parameters
+    public static Collection a() {
+        return Arrays.asList(new Object[][]{
+                { ItemType.NEW, 5, 0 },
+                { ItemType.SECOND_FREE, 1, 0 },
+                { ItemType.SECOND_FREE, 4, 50 },
+                { ItemType.SALE, 5, 70 },
+                { ItemType.REGULAR, 5, 0 },
+                { ItemType.REGULAR, 20, 2 },
+                { ItemType.REGULAR, 500, 50 },
+                { ItemType.REGULAR, 1000, 80 },
+        });
+    }
+
+    @Test
+    public void testCalculateDiscount() {
+        assertEquals(expectedDiscount, ShoppingCart2.calculateDiscount(itemType, quantity));
+    }
+}
