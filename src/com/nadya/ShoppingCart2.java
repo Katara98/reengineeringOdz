51b3e3e191a220acb15866b67b037a450c991605
Nadya <Katara98Katara@gmail.com>
2019-12-22 14:01:03 +0200

    Task #2: Encapsulate Item fields

--- src/com/nadya/ShoppingCart2.java
+++ src/com/nadya/ShoppingCart2.java
@@ -190,15 +190,47 @@ public class ShoppingCart2 {
      * item info
      */
     private static class Item {
-        String title;
-        double price;
-        int quantity;
-        ItemType type;
+        private String title;
+        private double price;
+        private int quantity;
+        private ItemType type;
 
         public Item(String title, double price, int quantity, ItemType type) {
             this.title = title;
             this.price = price;
             this.quantity = quantity;
+            this.type = type;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+
+        public ItemType getType() {
+            return type;
+        }
+
+        public void setType(ItemType type) {
             this.type = type;
         }
     }
